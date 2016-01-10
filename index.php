138fb9f9c2afe3a1b7ed3c41cba8d3693df2d488
williamhu3@gmail.com <williamhu3@gmail.com>
2016-01-09 17:17:59 -0800

    first version

--- index.php
+++ index.php
@@ -8,23 +8,54 @@
     
     <body>
         <?php
+        $position = $_GET['board'];
+        //testing using var_dump
+        //var_dump($position);
+          $squares = str_split($position);
+         // var_dump($squares);
+        if (winner('x',$squares)) echo 'You win.';
+        else if (winner('o',$squares)) echo 'I win.';
+        else echo 'No winner yet.';
+               
+ function winner($token,$position) {
+    $won = false;
+    if  (($position[0]==$token) && ($position[1] == $token) && ($position[2]==$token))    
+    {
+    $won = true;
+    } 
+    else if  (($position[3] ==$token) && ($position[4] ==$token) && ($position[5]==$token))
+        {
+        $won = true;
+        }    
+     else if  (($position[6] ==$token) && ($position[7] ==$token) && ($position[8]==$token))
+        {
+        $won = true;
+        }    
+        else if ( ($position[0] ==$token) && ($position[3] ==$token) && ($position[6]==$token))
+        {
+        $won = true;
+        }    
+         else if ( ($position[1] ==$token) && ($position[4] ==$token) && ($position[7]==$token))
+        {
+        $won = true;
+        } 
+         else if ( ($position[2] ==$token) && ($position[5] ==$token) && ($position[8]==$token))
+        {
+        $won = true;
+        } 
+         else if ( ($position[0] ==$token) && ($position[4] ==$token) && ($position[8]==$token))
+        {
+        $won = true;
+        } 
+        else if  (($position[2] ==$token) && ($position[4] ==$token) && ($position[6]==$token))
+        {
+        $won = true;
+        } 
+        
+ return $won;
+}
 
         ?>
     </body>
-    
-    function winner($token,$position) {
-    $won = false;
-    if  (($position[0]==$token) && 
-        ($position[1] == $token) &&
-        ($position[2]==$token))    {
-
-    $won == true;
-        }  else if  ($position[3] ==$token) &&
-              ($postition[4] ==$token) &&
-              ($position[5]==$token))
-        {
- //add more conditionals
- }    
- return $won;
-}
 </html>
+
