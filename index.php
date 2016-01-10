99f85b8d00ddd93a8877ce8d73a54f1bf297f1e4
williamhu3@gmail.com <williamhu3@gmail.com>
2016-01-09 23:59:32 -0800

    updated to incorporate game class

--- index.php
+++ index.php
@@ -7,53 +7,23 @@
     </head>
     
     <body>
-        <?php
+        <?php 
+        require 'Game.php';
+       //global
         $position = $_GET['board'];
         //testing using var_dump
         //var_dump($position);
-          $squares = str_split($position);
+         //$squares = str_split($position);
          // var_dump($squares);
-        if (winner('x',$squares)) echo 'You win.';
-        else if (winner('o',$squares)) echo 'I win.';
-        else echo 'No winner yet.';
+        $game = new Game($position);
+        $game ->display();
+          if ($game->winner('x'))
+              echo 'You win. Lucky guesses!';
+         else if ($game->winner('o'))
+             echo 'I win. Muwahaha';
+         else
+            echo 'No Winner yet, but y are losing.';
                
- function winner($token,$position) {
-    $won = false;
-    if  (($position[0]==$token) && ($position[1] == $token) && ($position[2]==$token))    
-    {
-    $won = true;
-    } 
-    else if  (($position[3] ==$token) && ($position[4] ==$token) && ($position[5]==$token))
-        {
-        $won = true;
-        }    
-     else if  (($position[6] ==$token) && ($position[7] ==$token) && ($position[8]==$token))
-        {
-        $won = true;
-        }    
-        else if ( ($position[0] ==$token) && ($position[3] ==$token) && ($position[6]==$token))
-        {
-        $won = true;
-        }    
-         else if ( ($position[1] ==$token) && ($position[4] ==$token) && ($position[7]==$token))
-        {
-        $won = true;
-        } 
-         else if ( ($position[2] ==$token) && ($position[5] ==$token) && ($position[8]==$token))
-        {
-        $won = true;
-        } 
-         else if ( ($position[0] ==$token) && ($position[4] ==$token) && ($position[8]==$token))
-        {
-        $won = true;
-        } 
-        else if  (($position[2] ==$token) && ($position[4] ==$token) && ($position[6]==$token))
-        {
-        $won = true;
-        } 
-        
- return $won;
-}
 
         ?>
     </body>
